e8d094bf3a0dfec19373850e36d2489df87d8e4c
Jatin Gupta <gjatin2233@gmail.com>
2026-01-21 15:56:29 +0530

    Implement message sending, message retrieval, and user listing for chat functionality.

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -2,5 +2,6 @@ import express from "express";
 const router = express.Router();
 router.get("/users", protectRoute, getUsersForSidebar)
 router.get("/:id", protectRoute, getMessages)
+router.post("/user/:id", protectRoute, sendMessage)
 
 export default router;
