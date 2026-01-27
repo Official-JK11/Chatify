c0bfd98ede3365826cf359cda2eb1b0c7bec9a1b
Jatin Gupta <gjatin2233@gmail.com>
2026-01-27 23:26:41 +0530

    some minor changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,35 +3,35 @@ import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
 
-export const signup = async  (req,res) =>{
+export const signup = async (req, res) => {
     try {
         if (!req.body) {
-        return res.status(400).json({ message: "Request body is missing" });
-    }
+            return res.status(400).json({ message: "Request body is missing" });
+        }
         const { fullName, email, password } = req.body
 
         if (!fullName || !email || !password) {
-        return res.status(400).json({ message: "All fields are required" });
-    }
-        if (password.length < 6) {
-            return res.status(400).json({message: "Password must be atleast 6 characters"})
+            return res.status(400).json({ message: "All fields are required" });
         }
-        const user = await User.findOne({email})
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be atleast 6 characters" })
+        }
+        const user = await User.findOne({ email })
 
-        if(user) return res.status(400).json({message: "Email already exists"});
+        if (user) return res.status(400).json({ message: "Email already exists" });
         const salt = await bcrypt.genSalt(10)
-        const hashPassword = await bcrypt.hash(password,salt)
+        const hashPassword = await bcrypt.hash(password, salt)
 
         const newUser = new User({
             fullName,
             email,
             password: hashPassword
         })
-        if (newUser){
+        if (newUser) {
             // generate jwt token here
             generateToken(newUser._id, res);
             await newUser.save();
-            
+
             res.status(201).json({
                 _id: newUser._id,
                 fullName: newUser.fullName,
@@ -39,62 +39,62 @@ export const signup = async  (req,res) =>{
                 profilePic: newUser.profilePic
             })
         } else {
-            res.status(400).json({message: "Invalid User"});
+            res.status(400).json({ message: "Invalid User" });
         }
 
     } catch (error) {
         console.log("Error in signup controller", error);
-        res.status(500).json({message: "Something is wrong in our system"})
+        res.status(500).json({ message: "Something is wrong in our system" })
     }
 }
 
-export const login = async (req,res) =>{
+export const login = async (req, res) => {
     try {
         if (!req.body) {
-        return res.status(400).json({ message: "Request body is missing" });
-    }
-    const { email, password } = req.body
-    if ( !email || !password) {
-        return res.status(400).json({ message: "All fields are required" });
-    }
-    const user = await User.findOne({email});
-    if(!user){
-        return res.status(400).json({message:"Invalid credentials"})
-    }
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
-    if (!isPasswordCorrect){
-        return res.status(400).json({message: "Invalid Password"})
-    }
-    generateToken(user._id,res)
-    return res.status(200).json({
-        _id: user._id,
-        fullName: user.fullName,
-        email: user.email,
-        profilePic: user.profilePic
-    })
+            return res.status(400).json({ message: "Request body is missing" });
+        }
+        const { email, password } = req.body
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid Password" })
+        }
+        generateToken(user._id, res)
+        return res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        })
     } catch (error) {
         console.log(`Error Message ${error}`);
-        return res.status(500).json({message:"Internal server error"})
+        return res.status(500).json({ message: "Internal server error" })
     }
 }
 
-export const logout = (req,res) =>{
+export const logout = (req, res) => {
     try {
-        res.cookie("jwt", "", {maxAge:0})
-        res.status(200).json({message:"Logged Out Successfully"})
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({ message: "Logged Out Successfully" })
     } catch (error) {
         console.log(`Error in logging out ${error}`);
-        res.status(500).json({message:"Internal Server Error"}) 
+        res.status(500).json({ message: "Internal Server Error" })
     }
 }
 
-export const updateProfile = async (req,res) =>{
+export const updateProfile = async (req, res) => {
     try {
         const { profilePic } = req.body;
         const userId = req.user._id
 
-        if(!profilePic){
-            return res.status(400).json({message: "Profile pic is required"})
+        if (!profilePic) {
+            return res.status(400).json({ message: "Profile pic is required" })
         }
 
         // Upload on cloudinary
@@ -103,26 +103,26 @@ export const updateProfile = async (req,res) =>{
         // Update User
         const updatedUser = await User.findByIdAndUpdate(
             userId,
-            { profilePic: uploadResponse.secure_url } ,
+            { profilePic: uploadResponse.secure_url },
             { new: true }
         )
 
         res.status(200).json(updatedUser)
     } catch (error) {
         console.log(`Update Profile Error: ${error}`);
-        return res.status(500).json({message: `Something is wrong in our system`})
+        return res.status(500).json({ message: `Something is wrong in our system` })
     }
 }
 
-export const checkAuth = (req,res) => {
+export const checkAuth = (req, res) => {
     try {
         if (!req.user) {
             return res.status(401).json({ message: "Unauthorized" });
         }
-       return res.status(200).json(req.user)
+        return res.status(200).json(req.user)
     } catch (error) {
         console.log(`CheckAuth Error: ${error}`);
-        return res.status(500).json({message: "Internal Server Error"});
-        
+        return res.status(500).json({ message: "Internal Server Error" });
+
     }
 }
